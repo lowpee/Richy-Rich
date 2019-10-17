8decc7c3fddd5963afe5928f7169d6d6fa7409f6
matt lopez <mattlopez.j@gmail.com>
2019-10-16 23:40:49 -0700

    Initial Commit

---
+++ Richy Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Richy Rich
+//
+//  Created by mattynil on 10/16/19.
+//  Copyright © 2019 mattynil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
